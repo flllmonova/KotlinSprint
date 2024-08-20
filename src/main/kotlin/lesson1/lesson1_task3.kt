247f437a944a66cfa9a9d6f23a5808e61407ff26
flllmonova <flllmonova@yandex.ru>
2024-08-21 00:40:31 +0500

    KS-1-3 refactor task 3

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -1,16 +1,16 @@
 package org.example.lesson1
 
 fun main() {
-    val year: UInt = 1961u
-    var hour: UInt = 9u
-    var minute: UInt = 7u
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
 
     println("Год полета в космос: $year")
-    println("Час взлета: $hour")
-    println("Минуты взлета: $minute")
+    println("Час взлета: 0$hour")
+    println("Минуты взлета: 0$minute")
 
-    hour = 10u
-    minute = 55u
+    hour = 10
+    minute = 55
 
     print("Время посадки: $hour:$minute")
 }
