90bbeb2a19701b5da30cd527cd16b53d58c32419
flllmonova <flllmonova@yandex.ru>
2024-08-20 22:31:28 +0500

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,6 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val name = "Kotlin"
-    println("Hello, " + name + "!")
+    println("Hello, $name!")
 }
