c69e50b13c01dd0404affa760341721fc039cca6
flllmonova <flllmonova@yandex.ru>
2024-08-23 19:41:45 +0500

    KS-2-1 do task_1

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson2
+
+fun main() {
+    val englishGradeofStudent1 = 3
+    val englishGradeofStudent2 = 4
+    val englishGradeofStudent3 = 3
+    val englishGradeofStudent4 = 5
+    val numberOfStudents = (4).toFloat()
+
+    val averageScoreInEnglish = (
+            englishGradeofStudent1 + englishGradeofStudent2 +
+                    englishGradeofStudent3 + englishGradeofStudent4) / numberOfStudents
+
+    println(averageScoreInEnglish)
+}
