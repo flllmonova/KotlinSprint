5ee49c6ff041090c1ad0626403c6fffcb7857abf
flllmonova <flllmonova@yandex.ru>
2024-08-25 01:01:50 +0500

    KS-2-4 refactor variable

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,13 +1,13 @@
 package org.example.lesson2
 
-const val BUFF_PERCENTAGE = 0.2f
+const val BUFF_PERCENTAGE = 20f
 
 fun main() {
     val crystalOreWithoutBuff = 7
-    val buffOfCrystalOre = (crystalOreWithoutBuff * BUFF_PERCENTAGE).toInt()
+    val buffOfCrystalOre = (crystalOreWithoutBuff * (BUFF_PERCENTAGE / 100)).toInt()
 
     val ironOreWithoutBuff = 11
-    val buffOfIronOre = (ironOreWithoutBuff * BUFF_PERCENTAGE).toInt()
+    val buffOfIronOre = (ironOreWithoutBuff * (BUFF_PERCENTAGE / 100)).toInt()
 
     println("Бафф к кристаллической руде составляет: $buffOfCrystalOre")
     println("Бафф к железной руде составляет: $buffOfIronOre")
