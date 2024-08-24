649b2c01302c16edf85d54a84722fc57effe0e82
flllmonova <flllmonova@yandex.ru>
2024-08-25 01:27:08 +0500

    KS-2-5 refactor output

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -6,9 +6,7 @@ fun main() {
     val rateOfInterest = 16.7
     val timeOfDeposit = 20
 
-    // формула сложного процента с ежегодной капитализацией
-    // итоговый_размер_вклада = сумма_вклада * (1 + процентная_ставка / 100) ^ срок_вклада
     val totalAmountOfDepositInRubles = amountOfDepositInRubles * (1 + rateOfInterest / 100).pow(timeOfDeposit)
 
-    System.out.printf("%.3f", totalAmountOfDepositInRubles)
+    println(String.format("%.3f", totalAmountOfDepositInRubles))
 }
