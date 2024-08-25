a8f5e6b26728d1067e4ccb51ac6ce0cf3d2e88af
flllmonova <flllmonova@yandex.ru>
2024-08-26 02:15:54 +0500

    KS-2-4 add variable and refactor expressions

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,13 +1,14 @@
 package org.example.lesson2
 
-const val BUFF_PERCENTAGE = 20f
+const val BUFF_PERCENTAGE = 20
+const val HUNDRED_SHARES = 100.0f
 
 fun main() {
     val crystalOreWithoutBuff = 7
-    val buffOfCrystalOre = (crystalOreWithoutBuff * (BUFF_PERCENTAGE / 100)).toInt()
+    val buffOfCrystalOre = (crystalOreWithoutBuff / HUNDRED_SHARES * BUFF_PERCENTAGE).toInt()
 
     val ironOreWithoutBuff = 11
-    val buffOfIronOre = (ironOreWithoutBuff * (BUFF_PERCENTAGE / 100)).toInt()
+    val buffOfIronOre = (ironOreWithoutBuff / HUNDRED_SHARES * BUFF_PERCENTAGE).toInt()
 
     println("Бафф к кристаллической руде составляет: $buffOfCrystalOre")
     println("Бафф к железной руде составляет: $buffOfIronOre")
