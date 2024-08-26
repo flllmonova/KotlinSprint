cf680816d527de065e9bc5aa16c93480bbe95195
flllmonova <flllmonova@yandex.ru>
2024-08-27 00:49:11 +0500

    KS-3-4 change output

--- src/main/kotlin/lesson3/lesson3_task4.kt
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -4,14 +4,14 @@ fun main() {
     var startPoint = "E2"
     var finalPoint = "E4"
     var moveNumber = 1
-    var playerMove = "$startPoint-$finalPoint;$moveNumber"
+    var playerMove = "[$startPoint-$finalPoint;$moveNumber]"
 
     println(playerMove)
 
     startPoint = "D2"
     finalPoint = "D3"
     moveNumber = 2
-    playerMove = "$startPoint-$finalPoint;$moveNumber"
+    playerMove = "[$startPoint-$finalPoint;$moveNumber]"
 
     println(playerMove)
 }
