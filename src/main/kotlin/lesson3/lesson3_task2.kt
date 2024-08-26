6028805f2957b6d318ea78c49d36498c3b5d36bb
flllmonova <flllmonova@yandex.ru>
2024-08-26 22:15:35 +0500

    KS-3-2 make function of output user data

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson3
+
+fun main() {
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    var lastName = "Андреева"
+    var age = 20
+
+    println("$lastName $firstName $middleName, $age")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("$lastName $firstName $middleName, $age")
+}
