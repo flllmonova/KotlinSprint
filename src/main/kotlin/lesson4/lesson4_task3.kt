3f5fdd795e822cee9f5f48988a02e84ed3ed7253
flllmonova <flllmonova@yandex.ru>
2024-08-28 19:09:10 +0500

    KS-4-3 refactor program

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,15 +1,21 @@
 package org.example.lesson4
 
+const val SUNNY_WEATHER = true
+const val OPEN_AWNING = true
 const val REQUIRED_AIR_HUMIDITY = 20
-const val REQUIRED_TIME_OF_YEAR = "не зима"
+const val UNFAVORABLE_TIME_OF_YEAR = "зима"
 
 fun main() {
-    val weatherIsSunny = true
-    val awningIsOpen = true
+    val isSunny = true
+    val isAwningOpen = true
     val airHumidity = 20
-    val currentTimeOFYear = "зима"
+    val currentTimeOfYear = "зима"
 
-    val analysisOfConditions = (weatherIsSunny) && awningIsOpen && (airHumidity == REQUIRED_AIR_HUMIDITY) && (currentTimeOFYear == REQUIRED_TIME_OF_YEAR)
+    val analysisOfConditions =
+        (SUNNY_WEATHER == isSunny) &&
+        (OPEN_AWNING == isAwningOpen) &&
+        (REQUIRED_AIR_HUMIDITY == airHumidity) &&
+        (UNFAVORABLE_TIME_OF_YEAR != currentTimeOfYear)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $analysisOfConditions")
 }
