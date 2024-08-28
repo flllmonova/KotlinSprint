c62845dda9bb0cedd8931c6eb46b958fb3e909c6
flllmonova <flllmonova@yandex.ru>
2024-08-29 00:43:45 +0500

    KS-4-3 rename variables

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,7 +1,7 @@
 package org.example.lesson4
 
-const val SUNNY_WEATHER = true
-const val OPEN_AWNING = true
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val REQUIRED_AIR_HUMIDITY = 20
 const val UNFAVORABLE_TIME_OF_YEAR = "зима"
 
@@ -12,8 +12,8 @@ fun main() {
     val currentTimeOfYear = "зима"
 
     val analysisOfConditions =
-        (SUNNY_WEATHER == isSunny) &&
-        (OPEN_AWNING == isAwningOpen) &&
+        (IS_SUNNY == isSunny) &&
+        (IS_AWNING_OPEN == isAwningOpen) &&
         (REQUIRED_AIR_HUMIDITY == airHumidity) &&
         (UNFAVORABLE_TIME_OF_YEAR != currentTimeOfYear)
 
