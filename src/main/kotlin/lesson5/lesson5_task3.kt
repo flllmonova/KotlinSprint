e4c33903dfee533cdcde1f5f2b113a0b0b9bb5f6
flllmonova <flllmonova@yandex.ru>
2024-09-03 17:47:43 +0500

    KS-5-3 add boolean operations

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -8,25 +8,19 @@ fun main() {
     println("Угадайте два числа от 0 до 42")
 
     println("Введите первое число:")
-    val firstNumber = readLine()!!.toInt()
+    val firstNumber = readln().toInt()
+    val isFirstNumberWinning = (firstNumber == FIRST_WINNING_NUMBER) || (firstNumber == SECOND_WINNING_NUMBER)
 
     println("Введите второе число:")
-    val secondNumber = readLine()!!.toInt()
+    val secondNumber = readln().toInt()
+    val isSecondNumberWinning = (secondNumber == FIRST_WINNING_NUMBER) || (secondNumber == SECOND_WINNING_NUMBER)
 
-    val isFirstNumberWinning = when {
-        firstNumber == FIRST_WINNING_NUMBER || firstNumber == SECOND_WINNING_NUMBER -> true
-        else -> false
-    }
-    val isSecondNumberWinning = when {
-        secondNumber == FIRST_WINNING_NUMBER || secondNumber == SECOND_WINNING_NUMBER -> true
-        else -> false
-    }
     val textResult = when {
-        (isFirstNumberWinning == true) && (isSecondNumberWinning == true) -> "Поздравляем! Вы выиграли главный приз!"
-        (isFirstNumberWinning == true) || (isSecondNumberWinning == true) -> "Вы выиграли утешительный приз!"
+        isFirstNumberWinning == true && isSecondNumberWinning == true -> "Поздравляем! Вы выиграли главный приз!"
+        isFirstNumberWinning == true || isSecondNumberWinning == true -> "Вы выиграли утешительный приз!"
         else -> "Неудача!"
     }
 
     println(textResult)
-    println("Для победы нужны были числа: $FIRST_WINNING_NUMBER и $SECOND_WINNING_NUMBER")
+    println("Выигрышные числа: $FIRST_WINNING_NUMBER и $SECOND_WINNING_NUMBER")
 }
