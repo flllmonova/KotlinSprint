221e208d93fe708beb8c3a531792f7f2d4288dd8
flllmonova <flllmonova@yandex.ru>
2024-09-04 03:09:35 +0500

    KS-6-5 make the program to verify the user

---
+++ src/main/kotlin/lesson6/lesson6_task5.kt
@@ -0,0 +1,33 @@
+package org.example.lesson6
+
+import kotlin.random.Random
+
+fun main() {
+    var attemptsNumber = 3
+    val beginningOfInterval = 1
+    val endOfInterval = 10
+
+    println("Для проверки что вы не бот, решите простой математический пример")
+
+    while (attemptsNumber > 0) {
+        val number1 = Random.nextInt(beginningOfInterval, endOfInterval)
+        val number2 = Random.nextInt(beginningOfInterval, endOfInterval)
+        val sumOfNumbers = number1 + number2
+
+        println("$number1 + $number2 = ?")
+
+        val userAnswer = readln().toInt()
+
+        if (userAnswer == sumOfNumbers) {
+            println("Добро пожаловать!")
+            break
+        } else {
+            --attemptsNumber
+            if (attemptsNumber == 0) {
+                println("Доступ запрещен")
+                break
+            }
+            println("Неверно. Попробуйте еще раз")
+        }
+    }
+}
