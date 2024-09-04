941620f00532a663e5b98dfa68aadff7994ae57d
flllmonova <flllmonova@yandex.ru>
2024-09-05 01:15:29 +0500

    KS-7-1 rename variable

--- src/main/kotlin/lesson7/lesson7_task1.kt
+++ src/main/kotlin/lesson7/lesson7_task1.kt
@@ -2,12 +2,12 @@ package org.example.lesson7
 
 fun main() {
     val intRange = 1..9
-    val charRange = 'a'..'z'
+    val lowercaseLettersRange = 'a'..'z'
     val passwordLength = 6
     var password = ""
 
     for (i in 1..passwordLength) {
-        password += if (i % 2 == 0) intRange.random() else charRange.random()
+        password += if (i % 2 == 0) intRange.random() else lowercaseLettersRange.random()
     }
 
     println(password)
