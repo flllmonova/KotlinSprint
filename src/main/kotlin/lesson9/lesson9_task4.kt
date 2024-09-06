d43ddcdc8fbba59f751fa9234494efc9c57648a9
flllmonova <flllmonova@yandex.ru>
2024-09-07 02:59:11 +0500

    KS-9-4 refactor code

--- src/main/kotlin/lesson9/lesson9_task4.kt
+++ src/main/kotlin/lesson9/lesson9_task4.kt
@@ -3,8 +3,7 @@ package org.example.lesson9
 fun main() {
 
     println("Напишите 5 ингредиентов через запятую c пробелом")
-    val ingredientsList = readln().split(", ")
-    val sortedIngredientList = ingredientsList.sorted()
+    val ingredientsList = readln().split(", ").sorted()
 
-    println(sortedIngredientList)
+    println(ingredientsList)
 }
