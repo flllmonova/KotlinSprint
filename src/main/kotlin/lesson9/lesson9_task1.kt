523f782b227f3cdbc31dd1ee78804ffd3f00a7f6
flllmonova <flllmonova@yandex.ru>
2024-09-06 18:38:39 +0500

    KS-9-1 update code

--- src/main/kotlin/lesson9/lesson9_task1.kt
+++ src/main/kotlin/lesson9/lesson9_task1.kt
@@ -1,14 +1,14 @@
 package org.example.lesson9
 
 fun main() {
-    val nameOfSalad = "Греческий"
-    val listOfSaladIngredients = listOf(
+
+    val ingredientsList = listOf(
         "огурец", "помидор", "перец болгарский", "листья салата", "маслины", "масло оливковое",
         "сыр фета", "лимон", "орегано", "перец душистый", "соль", "сахар", "чеснок"
     )
 
-    println("В рецепте салата $nameOfSalad есть следующие ингредиенты:")
-    listOfSaladIngredients.forEach { ingredient: String ->
+    println("В рецепте есть следующие ингредиенты:")
+    ingredientsList.forEach { ingredient: String ->
         println(ingredient)
     }
 }
