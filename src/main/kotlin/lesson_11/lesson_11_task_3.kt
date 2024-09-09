e9e9129a49e890cbd67f10978d3879e8a303d78f
flllmonova <flllmonova@yandex.ru>
2024-09-10 04:06:39 +0500

    KS-11-3 make classes Room and Participant

---
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -0,0 +1,40 @@
+package org.example.lesson_11
+
+fun main() {
+    val divingRoom = Room("images/covers/divers", "Дайвинг")
+}
+
+class Room(
+    val cover: String,
+    val name: String,
+) {
+    val participantList: MutableList<Participant> = mutableListOf()
+
+    fun addParticipant(newParticipant: Participant) {
+        participantList.add(newParticipant)
+        println("Добавлен участник ${newParticipant.nickname}")
+    }
+
+    fun pressOnParticipantAvatar(newParticipant: Participant) {
+        println("Участник ${newParticipant.nickname}")
+        println("статус: ${newParticipant.status}")
+    }
+
+    fun turnOffMicrophoneOfParticipant(participant: Participant, ) {
+        participant.status = "микрофон выключен"
+    }
+
+    fun turnOnMicrophone(participant: Participant) {
+        participant.status = "разговаривает"
+    }
+
+    fun turnOffSoundForParticipant(participant: Participant) {
+        participant.status = "пользователь заглушен"
+    }
+}
+
+class Participant(
+    val avatar: String,
+    val nickname: String,
+    var status: String = "микрофон выключен",
+)
