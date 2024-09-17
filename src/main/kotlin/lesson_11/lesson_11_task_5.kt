fb88e73d697956f1696cb34048b639cb5ab36313
flllmonova <flllmonova@yandex.ru>
2024-09-18 02:38:22 +0500

    KS-11-5 make a forum

---
+++ src/main/kotlin/lesson_11/lesson_11_task_5.kt
@@ -0,0 +1,58 @@
+package org.example.lesson_11
+
+fun main() {
+    val forum = Forum()
+
+    forum.createNewUser("Михаил")
+    forum.createNewUser("Кирилл")
+
+    forum.createNewMessage(1)
+    forum.createNewMessage(2)
+    forum.createNewMessage(1)
+    forum.createNewMessage(2)
+
+    forum.printThread()
+}
+
+class Forum {
+    var idGenerator = 1
+    val forumMembersList: MutableList<ForumMember> = mutableListOf()
+    val messageHistory: MutableList<String> = mutableListOf()
+
+    fun createNewUser(userName: String): ForumMember {
+        val newForumMember = ForumMember(idGenerator++, userName)
+        forumMembersList.add(newForumMember)
+        println("Пользователь $userName успешно зарегистрирован на форуме.")
+        return newForumMember
+    }
+
+    fun createNewMessage(userId: Int) {
+        if (userId !in 1..idGenerator) {
+            println("Вы не зарегистрированы на форуме")
+            return
+        }
+
+        println("Напишите сообщение:")
+        val message = ForumMessage(userId, readln())
+        forumMembersList.forEach{ member: ForumMember ->
+            if (member.userId == userId) messageHistory.add("${member.userName}: ${message.text}")
+        }
+    }
+
+    fun printThread() {
+        println("\nВывод всех сообщений форума:")
+        messageHistory.forEach{ message: String ->
+            println(message)
+        }
+    }
+}
+
+class ForumMember(
+    val userId: Int,
+    val userName: String,
+)
+
+class ForumMessage(
+    val authorId: Int,
+    val text: String,
+)
