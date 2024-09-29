6488fe83f27505c216a8929f76bf6ee016062434
flllmonova <flllmonova@yandex.ru>
2024-09-30 01:31:47 +0500

    KS-16-3 add method of validation to class

--- src/main/kotlin/lesson_16/lesson_16_task_3.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_3.kt
@@ -3,7 +3,6 @@ package org.example.lesson_16
 fun main() {
     val user1 = User("Standish")
     user1.setPassword()
-    validatePassword(user1.getPassword())
 }
 
 class User(
@@ -15,16 +14,15 @@ class User(
     fun setPassword() {
         println("Установите пароль:")
         password = readln()
-
     }
 
-    fun getPassword(): String = password
-}
+//    fun getPassword(): String = password
 
-fun validatePassword(password: String) {
-    val charsContOfRelevantPassword = 7
-    println(
-        if (password.length >= charsContOfRelevantPassword) "Пароль релевантен"
-        else "Пароль не релевантен. Его длина должна быть не менее $charsContOfRelevantPassword символов"
-    )
+    fun validatePassword(password: String) {
+        val charsContOfRelevantPassword = 7
+        println(
+            if (password.length >= charsContOfRelevantPassword) "Пароль релевантен"
+            else "Пароль не релевантен. Его длина должна быть не менее $charsContOfRelevantPassword символов"
+        )
+    }
 }
