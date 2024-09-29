0bd6d467e1e4b3cd1e21bafcc7e729cc926a2909
flllmonova <flllmonova@yandex.ru>
2024-09-30 03:06:23 +0500

    KS-16-3 change method of password validation

--- src/main/kotlin/lesson_16/lesson_16_task_3.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_3.kt
@@ -4,6 +4,8 @@ fun main() {
     val user1 = User("Standish")
     user1.setPassword()
 
+    println("Введите пароль снова:")
+    println("Валидность пароля: ${user1.validatePassword(readln())}")
 }
 
 class User(
@@ -17,8 +19,7 @@ class User(
         password = readln()
     }
 
-    fun validatePassword(_password: String) {
-        if (password == _password) println("Пароль введен верно")
-        else println("Пароль введен не верно")
+    fun validatePassword(_password: String): Boolean {
+        return _password == password
     }
 }
