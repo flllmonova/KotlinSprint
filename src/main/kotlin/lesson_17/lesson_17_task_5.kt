b55c40e5300cbcec208fb12201c189abe5ad8941
flllmonova <flllmonova@yandex.ru>
2024-10-02 02:00:13 +0500

    KS-17-5 make class User

---
+++ src/main/kotlin/lesson_17/lesson_17_task_5.kt
@@ -0,0 +1,31 @@
+package org.example.lesson_17
+
+fun main() {
+    val user = User("Itra", "fwj_798_SWF")
+    user.run {
+        println(login)
+        login = "Ra"
+        println(login)
+        println(password)
+        password = "adf_124"
+    }
+}
+
+class User(
+    _login: String,
+    _password: String,
+) {
+    var login = _login
+        set(value) {
+            field = value
+            println("Логин успешно обновлен")
+        }
+
+    var password = _password
+        get() {
+            return Array(field.length) { "*" }.joinToString("")
+        }
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+}
