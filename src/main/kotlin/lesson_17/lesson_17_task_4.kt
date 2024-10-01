527ec4623d03ba3b2bbedb22f48d547b59397ace
flllmonova <flllmonova@yandex.ru>
2024-10-02 01:34:01 +0500

    KS-17-4 make class Parcel

---
+++ src/main/kotlin/lesson_17/lesson_17_task_4.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_17
+
+fun main() {
+    val parcel = Parcel(562455695, "Москва")
+    parcel.getNumberOfParcelMoves()
+    parcel.parcelCurrentLocation = "Уфа"
+    parcel.getNumberOfParcelMoves()
+}
+
+class Parcel(
+    val parcelNumber: Long,
+    _parcelCurrentLocation: String,
+) {
+    private var movementCounter: Int = 0
+
+    fun getNumberOfParcelMoves() {
+        println("Число перемещений: $movementCounter")
+    }
+
+    var parcelCurrentLocation = _parcelCurrentLocation
+        set(value) {
+            field = value
+            ++movementCounter
+        }
+}
