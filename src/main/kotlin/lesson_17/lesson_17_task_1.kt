1d9c1ff3504e4917d736ebf9dbaf1e58cff1f748
flllmonova <flllmonova@yandex.ru>
2024-10-01 23:50:09 +0500

    KS-17-1 make QuizElement class

---
+++ src/main/kotlin/lesson_17/lesson_17_task_1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_17
+
+class QuizElement(
+    _question: String,
+    _answer: String,
+) {
+    val question = _question
+        get() = field
+    var answer = _answer
+        get() = field
+        set(value) { field = value }
+}
