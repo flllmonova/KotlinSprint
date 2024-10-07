8c0c21e705471ff9cf96649397cfa41dffb163af
flllmonova <flllmonova@yandex.ru>
2024-10-08 04:54:06 +0500

    KS-19-5 add class CardFile

--- src/main/kotlin/lesson_19/lesson_19_task_5.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_5.kt
@@ -1,64 +1,12 @@
 package org.example.lesson_19
 
 fun main() {
-    val cardsAmount = 5
-    val minNameLength = 3
-    val cardFile: MutableList<Person> = mutableListOf()
-
-    println("""
-        Добро пожаловать в приложение "Картотека"!
-        Инструкция по заполнению карточки:
-        - не использовать специальные символы в имени;
-        - имя следует писать с заглавной буквы, остальные буквы - строчные; 
-        - минимальное количество символов в имени: $minNameLength;
-        - для указания пола введите соответствующую заглавную кириллическую букву: "М" / "Ж".
-        
-    """.trimIndent())
-
-    do {
-        var name = ""
-        println("Введите имя:")
-        do {
-            name = readln()
-            val specialCharsRange = ' '..'/'
-
-            if (name.length >= minNameLength &&
-                name.all { it !in specialCharsRange } &&
-                name[0].isUpperCase() &&
-                name.drop(1).all { it.isLowerCase() }) {
-                break
-            } else {
-                println("Имя введено неверно, введите снова")
-            }
-        } while (true)
-
-        println("Введите пол:")
-
-        do {
-            val genderInput = readln()
-            val listOfGenderAbbreviatedForm = Gender.entries.map { it.abbreviatedFormOfGender }
-
-            if (genderInput in listOfGenderAbbreviatedForm) {
-                val gender = if (genderInput == Gender.MAN.abbreviatedFormOfGender) Gender.MAN else Gender.WOMAN
-                println("Карточка создана!")
-                cardFile.add(Person(name, gender))
-                break
-            } else println("Пол введен неверно, введите еще раз:")
-        } while(true)
-
-        if (cardFile.size %cardsAmount == 0) {
-            println()
-            printCardFile(cardFile)
-            println()
-        }
-
-        println("Желаете заполнить еще одну карточку? [ да / нет ]")
-        if(readln().equals("нет", ignoreCase = true)) {
-            println("Приложение \"Картотека\" закрывается")
-            break
-        }
-
-    } while (true)
+    val cardFile = CardFile()
+    cardFile.run {
+        greeting()
+        getInstructionToFillOutCard()
+        fillOutCard()
+    }
 }
 
 enum class Gender(val abbreviatedFormOfGender: String) {
@@ -76,10 +24,79 @@ class Person(
     val gender: Gender,
 )
 
-fun printCardFile(cardFile: List<Person>) {
-    println("Идет печать картотеки:")
-    cardFile.forEach { card: Person ->
-        println("Имя: ${card.name}, пол: ${card.gender.getFullFormOfGender()}")
-        Thread.sleep(500)
+class CardFile {
+    private val cardFile: MutableList<Person> = mutableListOf()
+
+    private val cardsAmountForPrint = 5
+    private val minNameLength = 3
+
+    fun greeting() {
+        println("Добро пожаловать в приложение \"Картотека\"!")
+    }
+
+    fun getInstructionToFillOutCard() {
+        println("""
+            Инструкция по заполнению карточки:
+            - не использовать специальные символы в имени;
+            - имя следует писать с заглавной буквы, остальные буквы - строчные; 
+            - минимальное количество символов в имени: $minNameLength;
+            - для указания пола введите соответствующую заглавную кириллическую букву: 
+            "${Gender.MAN.abbreviatedFormOfGender}" / "${Gender.WOMAN.abbreviatedFormOfGender}".
+        
+        """.trimIndent())
+    }
+
+    fun fillOutCard() {
+        do {
+            var name = ""
+            println("Введите имя:")
+            do {
+                name = readln()
+                val specialCharsRange = ' '..'/'
+
+                if (name.length >= minNameLength &&
+                    name.all { it !in specialCharsRange } &&
+                    name[0].isUpperCase() &&
+                    name.drop(1).all { it.isLowerCase() }) {
+                    break
+                } else {
+                    println("Имя введено неверно, введите снова")
+                }
+            } while (true)
+
+            println("Введите пол:")
+
+            do {
+                val genderInput = readln()
+                val listOfGenderAbbreviatedForm = Gender.entries.map { it.abbreviatedFormOfGender }
+
+                if (genderInput in listOfGenderAbbreviatedForm) {
+                    val gender = if (genderInput == Gender.MAN.abbreviatedFormOfGender) Gender.MAN else Gender.WOMAN
+                    println("Карточка создана!")
+                    cardFile.add(Person(name, gender))
+                    break
+                } else println("Пол введен неверно, введите еще раз:")
+            } while(true)
+
+            if (cardFile.size %cardsAmountForPrint == 0) {
+                println()
+                printCardFile()
+                println()
+            }
+
+            println("Желаете заполнить еще одну карточку? [ да / нет ]")
+            if(readln().equals("нет", ignoreCase = true)) {
+                println("Приложение \"Картотека\" закрывается")
+                break
+            }
+        } while (true)
+    }
+
+    fun printCardFile() {
+        println("Идет печать картотеки:")
+        cardFile.forEach { card: Person ->
+            println("Имя: ${card.name}, пол: ${card.gender.getFullFormOfGender()}")
+            Thread.sleep(500)
+        }
     }
 }
