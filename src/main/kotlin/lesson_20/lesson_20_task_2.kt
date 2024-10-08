c17cdbe14cc360f709bc49edeb68ae0a985c1542
flllmonova <flllmonova@yandex.ru>
2024-10-08 18:15:45 +0500

    KS-20-2 make lambda expression in variable healing potion and class Player

---
+++ src/main/kotlin/lesson_20/lesson_20_task_2.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_20
+
+fun main() {
+    val player = Player("Bruno", 50, 100)
+    healingPotion(player)
+}
+
+class Player(
+    private val name: String,
+    private var currentHealth: Int,
+    private val maxHealth: Int,
+) {
+    fun restoreCurrentHealth(valueHealth: Int) {
+        currentHealth = valueHealth
+    }
+
+    fun getMaxHealth() = maxHealth
+}
+
+val healingPotion: (player: Player) -> Unit = { it.restoreCurrentHealth(it.getMaxHealth()) }
