f62ea696d2ef52ff250d9097d0e30826bc9ae700
flllmonova <flllmonova@yandex.ru>
2024-10-17 00:44:36 +0500

    KS-22-1 override method equals

--- src/main/kotlin/lesson_22/lesson_22_task_1.kt
+++ src/main/kotlin/lesson_22/lesson_22_task_1.kt
@@ -8,16 +8,19 @@ fun main() {
     val dataBook1 = DataBook("Занимательная электротехника", "Фудзита Горо")
     val dataBook2 = DataBook("Занимательная электротехника", "Фудзита Горо")
 
-    println(book1 == book2)
+    println(book1.equals(book2))
     // в объектах обычного класса сравниваются ссылки, ссылки объектов разные => false
-    println(dataBook1 == dataBook2)
+    println(dataBook1.equals(dataBook2))
     // в объектах data класса сравниваются значения свойств, в примере содержимое объектов одинаковое => true
 }
 
 class RegularBook(
     val bookName: String,
     val author: String,
-)
+) {
+    override fun equals(other: Any?): Boolean = this === other
+}
+
 
 data class DataBook(
     val bookName: String,
